25bbbff5753f5dcba541871f0ed937193525fe2d
seydagulcn <seydagulcn@gmail.com>
2025-12-27 23:19:30 +0300

    girise gecerli ogrenci numaralari eklendi

--- src/seydaproje/Student.java
+++ src/seydaproje/Student.java
@@ -2,19 +2,38 @@ package seydaproje;
 
 public class Student {
     private String ogrenciNo;
-
     public String kimlikDogrula(String no) {
-        if (no != null && no.length() == 9 && no.matches("[0-9]+")) {
-            this.ogrenciNo = no;
-            return "true";
-        }else if(no.matches("[A-Za-z]+")) {
-        	return "sadece sayi girmelisiniz.";
-        }
-        else {return "ogrenci no 9 karakter olmali";
-        	
-        }
+    	
+
+    	   
+    	    String[] gecerliOgrenciler = {
+    	        "202012345",
+    	        "202045678",
+    	        "202078901",
+    	        "202011223",
+    	        "202099887"
+    	    };
+
+
+    	    if (no.matches("[A-Za-z]+")) {
+    	        return "sadece sayi girmelisiniz.";
+    	    }
+
+    	 
+    	    if (!no.matches("[0-9]+") || no.length() != 9) {
+    	        return "ogrenci no 9 karakter olmali";
+    	    }
+
+    	    for (String ogrNo : gecerliOgrenciler) {
+    	        if (ogrNo.equals(no)) {
+    	            this.ogrenciNo = no;
+    	            return "true";
+    	        }
+    	    }
+
+    	    return "ogrenci numarasi sistemde kayitli degil";
+    	}
         
-    }
 
     public String getOgrenciNo() { return ogrenciNo; }
 }
